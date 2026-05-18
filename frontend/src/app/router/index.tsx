6ae3d26d28d7928f33b9faa701aefa3fb37b5608
saohexaprime <sao.hexaprime@gmail.com>
2026-05-18 08:26:14 +0000

    updated

--- frontend/src/app/router/index.tsx
+++ frontend/src/app/router/index.tsx
@@ -31,7 +31,7 @@ export const router = createBrowserRouter([
     {
         path: "/",
         element: <LandingPage />,
-    },git stu
+    },
     {
         path: "/app",
         element: <DashboardLayout />,
